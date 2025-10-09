b9acbb7abb18209e69245cac3f47e2ad7edcc07d
Никита Курашов <nice.kurashov@mail.ru>
2025-10-10 00:40:35 +0800

    fix MembershipController(cors)

--- app/Http/Controllers/Membership/MembershipController.php
+++ app/Http/Controllers/Membership/MembershipController.php
@@ -84,7 +84,7 @@ class MembershipController extends Controller
                 'transaction_id' => $transaction->id
             ]);
 
-            return redirect()->away($link);
+            return redirect($link);
         }
         // return redirect()->away(route('membership.payment.success', ['transaction' => $transaction->id]));
     }
