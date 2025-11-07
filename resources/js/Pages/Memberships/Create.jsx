29a5ef17039b7782b3f6b066e57bde6a6b12cefb
Курашов Никита Алексеевич <22065@iat.iat>
2025-11-07 17:16:56 +0800

    fix

--- resources/js/Pages/Memberships/Create.jsx
+++ resources/js/Pages/Memberships/Create.jsx
@@ -3,7 +3,6 @@ import Header from "@/Components/layout/Header";
 import { useState } from "react";
 import { router } from "@inertiajs/react";
 
-
 export default function Create({ auth, membershipType }) {
 
     const [selectedMonth, setSelectedMonth] = useState(1);
@@ -44,12 +43,7 @@ export default function Create({ auth, membershipType }) {
                                 <ul className="space-y-3">
                                     {membershipType.features.filter(feature => feature.included).map(feature => (
                                         <li key={feature.id} className="flex items-center space-x-2">
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                className="h-4 w-4 text-green-500"
-                                                viewBox="0 0 20 20"
-                                                fill="currentColor"
-                                            >
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-green-500" viewBox="0 0 20 20" fill="currentColor">
                                                 <path
                                                     fillRule="evenodd"
                                                     d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
